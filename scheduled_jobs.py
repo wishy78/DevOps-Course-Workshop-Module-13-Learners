ed0f23778bf48b2264f9e8dc7fad28d4040069d8
Jonathan Longbone <jon.longbone@apdcomms.co.uk>
2023-03-16 11:44:55 +0000

    modified Time

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -27,7 +27,7 @@ def process_orders(app):
         payload = {
             "product": order.product,
             "customer": order.customer,
-            "date": order.date_placed.isoformat(),
+            "date": order.date_placed_local.isoformat(),
         }
 
         response = requests.post(
